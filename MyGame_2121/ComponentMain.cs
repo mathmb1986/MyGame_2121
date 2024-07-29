e34bdf62e592c27fa4faa08b19d6a04483eb9df1
Mathmb1986 <52980023+mathmb1986@users.noreply.github.com>
2024-07-29 17:36:08 -0400

    Add files via upload

---
+++ MyGame_2121/ComponentMain.cs
@@ -0,0 +1,18 @@
+﻿using Stride.Core;
+using Stride.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extension
+{
+    [ComponentCategory("MyCategoryMain")]
+    [DataContract]
+    public class ComponentTEst : StartupScript
+    {
+
+
+    }
+}
